e140feb4c197260bae057e963ddbfeab37183228
Navian <primeguyy@gmail.com>
2021-06-12 20:00:13 +0000

    navigation component added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import HomeScreen from "./screens/HomeScreen";
 function App() {
   return (
     <div className="app">
-      <h1>Ready for the Netflix Clone???</h1>
       <HomeScreen />
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,38 @@
+import { useState, useEffect } from "react";
 import "./Nav.css";
 
 const Nav = () => {
+  const [animateNav, setAnimateNav] = useState(false);
+
+  // handle navbar animation
+  const handleNavAnimation = () => {
+    if (window.scrollY > 100) {
+      setAnimateNav(true);
+    } else {
+      setAnimateNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleNavAnimation);
+    return () => window.addEventListener("scroll", handleNavAnimation);
+  }, []);
+
   return (
-    <nav className="nav">
-      <h3>Some nav content</h3>
+    <nav className={`nav ${animateNav && "nav__black"}`}>
+      <div className="nav-contents">
+        <img
+          src="images/netflix-logo.png"
+          alt="netflix logo"
+          className="nav-logo"
+        />
+
+        <img
+          src="/images/nav-avatar.png"
+          alt="user avatar"
+          className="nav-avatar"
+        />
+      </div>
     </nav>
   );
 };
