0577dbba5c87817bc97cb5f6b937bdeef22f44d2
Navian <primeguyy@gmail.com>
2021-06-17 16:45:18 +0000

    intro text and user input field added

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import "./LoginScreen.css";
 
 const LoginScreen = () => {
+  const [signedIn, setSignedIn] = useState(false);
+
+  function handleSignIn(e) {
+    e.preventDefault();
+
+    setSignedIn(true);
+  }
+
   return (
     <header className="loginScreen">
       <div className="loginScreen-nav">
@@ -12,10 +21,30 @@ const LoginScreen = () => {
           />
         </div>
 
-        <button className="signIn-btn">Sign In</button>
+        <button className="signIn-btn" onClick={() => setSignedIn(true)}>
+          Sign In
+        </button>
 
         <div className="gradient-layer" />
       </div>
+
+      <section className="loginScreen-body">
+        <div>
+          <h1 className="main-text">Unlimited movies, TV shows, and more.</h1>
+          <h2 className="sub-text">Watch anywhere. Cancel anytime.</h2>
+          <h3 className="cta__sub-text">
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </h3>
+
+          <form className="cta-form">
+            <input type="text" className="email" placeholder="Email address" />
+            <button className="cta-started" onClick={handleSignIn}>
+              Get Started &rarr;
+            </button>
+          </form>
+        </div>
+      </section>
     </header>
   );
 };
